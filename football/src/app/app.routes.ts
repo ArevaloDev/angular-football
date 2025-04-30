5ae4dd3d8443aff288c990f8538fa41a9aeaf25f
ArevaloDev <arevalodaniel999@gmail.com>
2025-04-29 22:03:07 -0600

    agregando rutas de la app

--- football/src/app/app.routes.ts
+++ football/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LeaguesComponent } from './components/leagues/leagues.component';
+import { TeamsComponent } from './components/teams/teams.component';
+import { PlayersComponent } from './components/players/players.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'ligas', component:LeaguesComponent},
+  {path: 'equipos', component:TeamsComponent},
+  {path: 'jugadores', component:PlayersComponent},
+  {path: '**', redirectTo: ''},
+];
