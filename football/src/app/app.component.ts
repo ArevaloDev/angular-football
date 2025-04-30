ca05a8a63c532557b1b1b6f3325b68ed8bd335c9
ArevaloDev <arevalodaniel999@gmail.com>
2025-04-29 20:46:15 -0600

    creando estilos y resolviendo error de creacion de servicios

--- football/src/app/app.component.ts
+++ football/src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from "./shared/navbar/navbar.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HomeComponent
-  ],
+    HttpClientModule,
+    HomeComponent,
+    NavbarComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
