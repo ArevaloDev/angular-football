8e784466ffb5e53c9b8c73f5a74775e53faba0a2
ArevaloDev <arevalodaniel999@gmail.com>
2025-04-29 22:02:26 -0600

    agregando al servicio el llamado de la api de ligas

--- football/src/app/services/football/football.service.ts
+++ football/src/app/services/football/football.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { enviroments } from '../../../enviroments/enviroments';
+import { map, tap } from 'rxjs';
+import { ResponseLeague } from '../../interfaces/leagues.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FootballService {
 
-  constructor() { }
+  private url:string = enviroments.urlAPI;
+  private apiKey:string = enviroments.apiKey;
+  constructor(private http:HttpClient) { }
+
+  getLeagues(){
+    return this.http.get<ResponseLeague>(`${this.url}/leagues`).pipe(
+      map(response => response.response)
+    )
+  }
 }
